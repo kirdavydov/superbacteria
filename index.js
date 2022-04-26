a777ce5ef602d250a33f29c93e60724ec6420df1
Kirill Davydov <kirilldavydov@Kirills-MacBook-Air.local>
2022-04-26 12:15:49 +0200

    testing

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ function scrollToElement(e){
   e.scrollIntoView({behavior: 'smooth'});
 }
 
-isHidden = false;
-sidebar = document.getElementById('sidebar')
-function hide(){
-  if(isHidden == false){
-    sidebar.style.left = '-400px'
-    isHidden = true;
-  }
-  else if(isHidden == true){
-    sidebar.style.left = '0px'
-    isHidden = false;
-  }
-}
+//isHidden = false;
+//sidebar = document.getElementById('sidebar')
+//function hide(){
+//  if(isHidden == false){
+//    sidebar.style.left = '-400px'
+//    isHidden = true;
+//  }
+//  else if(isHidden == true){
+//    sidebar.style.left = '0px'
+//    isHidden = false;
+//  }
+//}
