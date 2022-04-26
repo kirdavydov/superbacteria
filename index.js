cbbee33e31626245e06b26e2e6ff2ee374352be8
Kirill Davydov <kirilldavydov@Kirills-MacBook-Air.local>
2022-04-26 12:21:26 +0200

    v2.0

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
 function scrollToElement(e){
   e.scrollIntoView({behavior: 'smooth'});
 }
-
-//isHidden = false;
-//sidebar = document.getElementById('sidebar')
-//function hide(){
-//  if(isHidden == false){
-//    sidebar.style.left = '-400px'
-//    isHidden = true;
-//  }
-//  else if(isHidden == true){
-//    sidebar.style.left = '0px'
-//    isHidden = false;
-//  }
-//}
